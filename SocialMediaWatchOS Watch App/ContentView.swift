037c307f52031d3608dcf473c4fefc304092f0f8
Batuhan Kabaktepe <utab.beta@gmail.com>
2024-09-21 09:52:53 +0200

    Initial Commit

---
+++ SocialMediaWatchOS Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialMediaWatchOS Watch App
+//
+//  Created by Batuhan Kabaktepe on 21.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialMediaWatchOS Watch App/SocialMediaWatchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaWatchOSApp.swift
+//  SocialMediaWatchOS Watch App
+//
+//  Created by Batuhan Kabaktepe on 21.09.24.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaWatchOS_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
